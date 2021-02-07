c18b0c0e76752ac0da1f921a8114c8217b1849c5
Cullen <cullenyanderson@gmail.com>
2021-02-07 13:18:38 -0500

    Website now displays analyzed images

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, url_for, render_template, redirect, send_file
+from flask.helpers import send_from_directory
 from werkzeug.utils import secure_filename
-import imghdr
 import os
 from tensorflow.keras.models import load_model
 from machinelearning import ml
@@ -11,21 +11,12 @@ import base64
 
 
 app = Flask(__name__)
-app.config['MAX_CONTENT_LENGTH'] = 2048 * 2048  # only accepts files up to 2 mb
-app.config['UPLOAD_EXTENSIONS'] = ['.jpeg', '.jpg', '.png']
+app.config['MAX_CONTENT_LENGTH'] = 6 * 1024 * 1024  # only accepts files up to 6 mb
+app.config['UPLOAD_EXTENSIONS'] = ['.jpeg', '.jpg', '.png', '.heic']
 app.config['UPLOAD_PATH'] = 'uploads'
 
 
 model = load_model("vgg16_emotion_detection.h5")
-
-
-def validate_image(stream):
-    header = stream.read(512)
-    stream.seek(0)
-    format = imghdr.what(None, header)
-    if not format:
-        return None
-    return '.' + (format if format != 'jpeg' else 'jpg')
 
 
 def serve_image(image):
@@ -34,11 +25,10 @@ def serve_image(image):
     a website
     '''
     image = Image.fromarray(image.astype('uint8'))
-    bytesio = io.BytesIO
-    image.save(bytesio, format='JPEG', quality=70)
-    bytesio.seek(0)
-    image = base64.b64encode(bytesio.getvalue())
-    return image
+    data = io.BytesIO()
+    image.save(data, 'JPEG')
+    encoded_img_data = base64.b64encode(data.getvalue())
+    return encoded_img_data
 
 
 @app.route('/')
@@ -46,28 +36,44 @@ def index():
     return render_template('index.html')
 
 
-# i put in a lot of security stuff here that isn't totally necessary for my purposes but why not
 @app.route('/', methods=["POST"])
 def upload_file():
+    print(model)
     file = request.files['file']
     filename = secure_filename(file.filename)
     if filename != '':
         file_ext = os.path.splitext(filename)[1].lower()
-        if file_ext not in app.config['UPLOAD_EXTENSIONS'] or file_ext != validate_image(file.stream):
-            return render_template('index.html', error='Please submit an image (.jpg, .jpeg, or .png)')
+        print(file_ext)
+        if file_ext not in app.config['UPLOAD_EXTENSIONS']: # or file_ext != validate_image(file.stream) might be source of some problems
+            return render_template('index.html', error='This image is not recognized. Please submit a valid image (jpg, jpeg, png).')
         basepath = os.path.dirname(__file__)
         file_path = os.path.join(basepath, app.config['UPLOAD_PATH'], filename)
         file.save(file_path)
-        predictions, face_locations = ml.make_predictions(model, file_path)
+        try:
+            predictions, face_locations, error = ml.make_predictions(model, file_path)
+            print(predictions, error)
+        except ValueError:
+            print("Value Error")
+            return render_template('index.html', error='The model is having trouble analyzing this image. (faces are not being properly sized) To be fixed.')
+        except Exception:
+            print("Exception")
+            return render_template('index.html', error='Something went wrong. To be fixed.')
         image = ml.draw_predictions(file_path, face_locations, predictions)
         # serve image is not working
-        image = serve_image(image)
-        return render_template('index.html', predictions=predictions, image=image.decode('ascii'))
+        encoded_img_data = serve_image(image)
+        if error is not None:
+            return render_template('index.html', error=error)
+        return render_template('index.html', predictions=predictions, img_data=encoded_img_data.decode('utf-8'))
     return redirect(url_for('index'))
 
 
+@app.route('/display/<filename>')
+def display_image(filename):
+    return send_from_directory(app.config['UPLOAD_PATH'], filename)
+
+
 if __name__ == "__main__":
-    app.run()
+    app.run(host='0.0.0.0')
 
 # im basically copying code for displaying the image
 # figure this out after the gym
@@ -76,3 +82,11 @@ if __name__ == "__main__":
 
 # learn more about os module. it always comes up but i am not familiar with it
 # learn more about this io stuff
+
+# def validate_image(stream):
+#     header = stream.read(512)
+#     stream.seek(0)
+#     format = imghdr.what(None, header)
+#     if not format:
+#         return None
+#     return '.' + (format if format != 'jpeg' else 'jpg')
--- machinelearning/ml.py
+++ machinelearning/ml.py
@@ -1,10 +1,10 @@
 import numpy as np
 import PIL
-import matplotlib.image as mpimg
 import face_recognition
 import cv2
 
 
+# no faces are detected on some images for some reason
 def get_faces(image_fp):
     '''
     Returns a list of faces identified in an image
@@ -13,14 +13,23 @@ def get_faces(image_fp):
         faces: list of numpy arrays containing faces identified from image
         face_locations: coordinates of faces in image
     '''
-    image = mpimg.imread(image_fp)
+    image = face_recognition.load_image_file(image_fp)
+    try:
+        print(image.shape)
+    except Exception:
+        pass
     faces = list()
     face_locations = face_recognition.face_locations(image)
     for i in range(len(face_locations)):
         top, right, bottom, left = face_locations[i]
         face_image = image[top:bottom, left:right]
         faces.append(face_image)
-    return faces, face_locations
+    if face_locations:
+        error = None
+        return faces, face_locations, error
+    else:
+        error = 'No faces were detected'
+        return faces, face_locations, error
 
 
 def process_faces(faces):
@@ -37,6 +46,10 @@ def process_faces(faces):
         face_image = face_image / 255
         face_image = np.expand_dims(face_image, axis=0)
         processed_faces.append(face_image)
+    try:
+        print(processed_faces[0].shape)
+    except IndexError:
+        print("Processed_faces is empty")
     return processed_faces
 
 
@@ -50,7 +63,7 @@ def make_predictions(model, image_fp):
         faces: a list containing the faces identified in the image as numpy arrays
         predictions: a list of predicted emotions from faces
     '''
-    faces, face_locations = get_faces(image_fp)
+    faces, face_locations, error = get_faces(image_fp)
     faces = process_faces(faces)
     emotion_labels = {0: 'Angry', 1: 'Disgust', 2: 'Fear', 3: 'Happy', 4: 'Sad', 5: 'Surprise', 6: 'Neutral'}
     predictions = list()
@@ -58,7 +71,7 @@ def make_predictions(model, image_fp):
         prediction = model.predict(face)
         emotion = emotion_labels[np.argmax(prediction)]
         predictions.append(emotion)
-    return predictions, face_locations
+    return predictions, face_locations, error
 
 
 def draw_predictions(image_fp, face_locations, predictions):
@@ -73,7 +86,7 @@ def draw_predictions(image_fp, face_locations, predictions):
         end.append((i[3], i[2]))
     for j in range(len(start)):
         image = cv2.rectangle(image, start[j], end[j], (36,255,12), 2)
-        cv2.putText(image, predictions[j], (end[j][0], start[j][1]+10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (36,255,12), 2)
+        cv2.putText(image, predictions[j], (end[j][0]+10, start[j][1]-10), cv2.FONT_HERSHEY_SIMPLEX, 1.5, (36,255,12), 2)
     return image
     
 # i could change this to return coordinates of faces instead and use open cv to display that
